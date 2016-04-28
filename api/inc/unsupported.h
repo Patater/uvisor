a8e2f15bcd72e254216e6a9bcece9ff794b34303
Niklas Hauser <niklas.hauser@arm.com>
2016-04-28 14:52:25 +0100

    Update box macros in unsupported.h

--- api/inc/unsupported.h
+++ api/inc/unsupported.h
@@ -66,6 +66,8 @@ UVISOR_EXTERN const uint32_t __uvisor_mode;
 
 #define UVISOR_BOX_NAMESPACE(...)
 
+#define UVISOR_BOX_HEAPSIZE(heap_size)
+
 /* uvisor-lib/interrupts.h */
 
 #define vIRQ_SetVector(irqn, vector)        NVIC_SetVector((IRQn_Type) (irqn), (uint32_t) (vector))
