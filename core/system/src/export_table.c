Patater/uvisor

commit 7408972eddd3a257c30fcaf15267ccef88263e89

export-table: Add export table
Whenever uVisor has a need to communicate symbols contained within the uVisor
binary to the application, it can use the export table.

uvisor_post_init() is also added. The system startup must call this to
initialize uvisor-lib things. In the initial implementation of
uvisor_post_init(), we check that the export table is what we expect from
uVisor. This keeps applications and uVisor binaries from getting out of sync
via a run-time check.

core/system/src/export_table.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2016, ARM Limited, All Rights Reserved
+ * Copyright (c) 2016, ARM Limited, All Rights Reserved
  * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -14,23 +14,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "uvisor-lib/uvisor-lib.h"
+#include "api/inc/export_table_exports.h"
 
-#if !(defined(UVISOR_PRESENT) && (UVISOR_PRESENT == 1))
-
-/* Note: This file is not included in the uVisor release library. Instead, the
- *       host OS needs to compile it separately if a platform does not support
- *       uVisor (but uVisor API header files are still used). */
-
-/* uVisor hook for unsupported platforms */
-UVISOR_EXTERN void uvisor_init(void)
-{
-    return;
-}
-
-int uvisor_lib_init(void)
-{
-    return 0;
-}
-
-#endif
+/* This table must be located at the end of the uVisor binary so that this
+ * table can be exported correctly. Placing this table into the .export_table
+ * section locates this table at the end of the uVisor binary. */
+__attribute__((section(".export_table")))
+static const TUvisorExportTable __uvisor_export_table = {
+    .magic = UVISOR_EXPORT_MAGIC,
+    .version = UVISOR_EXPORT_VERSION,
+    .size = sizeof(TUvisorExportTable)
+};

Makefile
@@ -102,6 +102,7 @@ SOURCES:=\
          $(CORE_SYSTEM_DIR)/src/stdlib.c \
          $(CORE_SYSTEM_DIR)/src/svc.c \
          $(CORE_SYSTEM_DIR)/src/unvic.c \
+         $(CORE_SYSTEM_DIR)/src/export_table.c \
          $(CORE_SYSTEM_DIR)/src/system.c \
          $(CORE_SYSTEM_DIR)/src/mpu/vmpu.c \
          $(CORE_DEBUG_DIR)/src/debug.c \

api/inc/export_table_exports.h
@@ -1,0 +1,44 @@
+/*
+ * Copyright (c) 2016, ARM Limited, All Rights Reserved
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef __UVISOR_API_EXPORT_TABLE_EXPORTS_H__
+#define __UVISOR_API_EXPORT_TABLE_EXPORTS_H__
+
+#include <stdint.h>
+
+/* If this magic doesn't match what you get in a TUvisorExportTable, then you
+ * didn't find a TUvisorExportTable and all bets are off as to what will be
+ * contained in what you found. */
+#define UVISOR_EXPORT_MAGIC 0x5C9411B4
+
+/* This is the export table API version. If this version doesn't match what you
+ * get in TUvisorExportTable, then you need a different header file to
+ * understand the TUvisorExportTable. */
+#define UVISOR_EXPORT_VERSION 0
+
+typedef struct {
+    /* magic and version must be present as the first two elements in this
+     * table so that across various versions of the table layout, the table can
+     * be interpreted correctly. */
+    uint32_t magic;
+    uint32_t version;
+
+    /* This must be the last element of the table so that uvisor-input.S can
+     * export the size statically. */
+    uint32_t size;
+} TUvisorExportTable;
+
+#endif

api/inc/uvisor-lib.h
@@ -41,10 +41,16 @@
 
 #endif /* defined(UVISOR_PRESENT) && UVISOR_PRESENT == 1 */
 
+/* The host startup needs to call this after osKernelInitialize to initialize
+ * uvisor-lib. The function can fail. It's up the the host startup to decide
+ * what to do with any failures. */
+UVISOR_EXTERN int uvisor_lib_init(void);
+
 /* Include all exported header files used by uVisor internally.
  * These are included independently on whether uVisor is supported or not by the
  * target platform. */
 #include "api/inc/debug_exports.h"
+#include "api/inc/export_table_exports.h"
 #include "api/inc/halt_exports.h"
 #include "api/inc/secure_gateway_exports.h"
 #include "api/inc/svc_exports.h"

api/src/uvisor-input.S
@@ -15,6 +15,7 @@
  * limitations under the License.
  */
 .globl uvisor_init
+.globl uvisor_export_table_size
 .globl uvisor_config
 .globl uvisor_ctx
 .type  uvisor_init, %function
@@ -28,6 +29,12 @@
 uvisor_init:
     /* uvisor - precompiled blob */
     .incbin UVISOR_BIN
+
+/* The size of the uVisor export table is a uint32_t located at the end of the
+ * uVisor binary. The uVisor export table is located at size number of bytes
+ * before the end of the uVisor binary. Size includes the 4 bytes for size
+ * itself. */
+.set uvisor_export_table_size, uvisor_config - 4
 
 uvisor_config:
     /* uvisor expects its configuration section right

api/src/uvisor-lib.c
@@ -1,0 +1,41 @@
+/*
+ * Copyright (c) 2016, ARM Limited, All Rights Reserved
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "api/inc/export_table_exports.h"
+
+int uvisor_lib_init(void)
+{
+    /* Supported */
+    /* Defined in uvisor-input.S */
+    extern uint32_t uvisor_config;
+    extern uint32_t uvisor_export_table_size;
+
+    uintptr_t uvisor_config_addr = (uintptr_t) &uvisor_config;
+
+    TUvisorExportTable * uvisor_export_table = (TUvisorExportTable *) (uvisor_config_addr - uvisor_export_table_size);
+
+    if (uvisor_export_table->magic != UVISOR_EXPORT_MAGIC) {
+        /* We couldn't find the magic. */
+        return -1;
+    }
+
+    if (uvisor_export_table->version != UVISOR_EXPORT_VERSION) {
+        /* The version we understand is not the version we found. */
+        return -1;
+    }
+
+    return 0;
+}

core/linker/default.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2015, ARM Limited, All Rights Reserved
+ * Copyright (c) 2013-2016, ARM Limited, All Rights Reserved
  * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -60,7 +60,7 @@ SECTIONS
         . = ALIGN(512);
         *(.isr*)
         PROVIDE(__data_start_src__ = LOADADDR(.data));
-        PROVIDE(__uvisor_config = LOADADDR(.data) + SIZEOF(.data));
+        PROVIDE(__uvisor_config = LOADADDR(.export_table) + SIZEOF(.export_table));
         PROVIDE(__stack_start__ = ORIGIN(STACK));
         PROVIDE(__stack_top__ = ORIGIN(STACK) + LENGTH(STACK) - STACK_GUARD_BAND);
         PROVIDE(__stack_end__ = ORIGIN(STACK) + LENGTH(STACK));
@@ -87,6 +87,13 @@ SECTIONS
         /* All data end */
         __data_end__ = .;
     } > RAM AT>FLASH
+
+    .export_table :
+    {
+        /* The __uvisor_export_table must be placed at the very end of the
+         * uVisor binary to work correctly. */
+        KEEP(*(.export_table))
+    } > FLASH
 
     .bss (NOLOAD):
     {

commit 052c893ed4eb67be934a331c80d96e23bb2fbb7c

fixup! export-table: Add export table
The original implementation of the export table had a bug where, only in
release builds, the export table was optimized out by the compiler. Make
the export table non-static so that the compiler will not optimize out the
export table in release builds.

Fixes: 7408972eddd3 ("export-table: Add export table")

core/system/src/export_table.c
@@ -20,7 +20,7 @@
  * table can be exported correctly. Placing this table into the .export_table
  * section locates this table at the end of the uVisor binary. */
 __attribute__((section(".export_table")))
-static const TUvisorExportTable __uvisor_export_table = {
+const TUvisorExportTable __uvisor_export_table = {
     .magic = UVISOR_EXPORT_MAGIC,
     .version = UVISOR_EXPORT_VERSION,
     .size = sizeof(TUvisorExportTable)