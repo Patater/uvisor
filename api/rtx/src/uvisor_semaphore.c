891ee9916296323cb50f633a8e87db0c37b6212a
Jaeden Amero <jaeden.amero@arm.com>
2016-08-04 14:57:52 +0100

    fix semaphore

--- api/rtx/src/uvisor_semaphore.c
+++ api/rtx/src/uvisor_semaphore.c
@@ -19,21 +19,8 @@ int uvisor_semaphore_init(uvisor_semaphore_t * s, int32_t count)
     semaphore->def.semaphore = semaphore->data;
     semaphore->id = osSemaphoreCreate(&semaphore->def, count);
 
-    s->next = NULL;
-
     /* Error when semaphore->id is NULL */
     return -(semaphore->id == NULL);
-}
-
-void uvisor_semaphore_list_append(uvisor_semaphore_t * semaphore_list, uvisor_semaphore_t * next)
-{
-    /* Find the last item in the list. */
-    while (semaphore_list->next != NULL) {
-        semaphore_list = semaphore_list->next;
-    }
-
-    /* Add the new semaphore to the end of the list. */
-    semaphore_list->next = next;
 }
 
 int uvisor_semaphore_pend(uvisor_semaphore_t * s, uint32_t timeout_ms)
